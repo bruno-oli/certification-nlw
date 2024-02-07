ab57334f74e2fa4463a858de1660405630871ab8
bruno-oli <brunomaz177@gmail.com>
2024-02-07 13:21:17 -0300

    first commit

---
+++ src/main/java/com/rocketseat/cetification_nlw/CetificationNlwApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.cetification_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetificationNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetificationNlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/cetification_nlw/CetificationNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.cetification_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetificationNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
